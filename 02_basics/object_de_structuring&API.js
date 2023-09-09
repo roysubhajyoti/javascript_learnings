fc08e33d23a905bd780464ba3a4082cbcd36cde4
roysubhajyoti <roysubbhajyoti272@gmail.com>
2023-09-09 12:16:12 +0530

    object_de_structuring and JASON API intro

---
+++ 02_basics/object_de_structuring&API.js
@@ -0,0 +1,32 @@
+//object de-structuring 
+
+const course={
+    name:"Oops",
+    courseInstructor:"NGB",
+    college:"HITK"
+}
+
+//now destrucring :
+//syntax : {propertyname : chnage_prop_name} =  ObjectName in which the property belong
+const {courseInstructor : Instructor}=course
+
+console.log(Instructor);
+
+
+//########################## JSON API #############
+//JSON
+
+// {
+//     "name":"Subhajyoti",
+//     "city":"kolkata"
+// }
+
+//API infrom of array of objects
+[
+    {},
+    {},
+    {}
+]
+
+//Radmon API --> for random jason api to study
+//Json Formatter --> to format it accordingly
