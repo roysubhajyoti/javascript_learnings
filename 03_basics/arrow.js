894fd227f5a7dfc00e35c12b6b999f1477ba8732
roysubhajyoti <roysubbhajyoti272@gmail.com>
2023-09-16 00:43:37 +0530

    arrow function : this ,new

--- 03_basics/arrow.js
+++ 03_basics/arrow.js
@@ -41,3 +41,71 @@ eg:--------------------------------------------
 3.this binding
  
  */
+
+const user = {
+    username:"Subhajyoti",
+    pass:56648,
+    welcomeMessage:function(){
+        console.log(`welcome welcome ,${this.username}`);
+
+        console.log(this);
+    }
+
+}
+user.welcomeMessage()
+//expected op: welcome welcome ,Subhajyoti
+
+user.username="Roy";
+user.welcomeMessage()
+//expected op:welcome welcome ,Roy
+
+console.log(user);
+/**{
+  username: 'Roy',
+  pass: 56648,
+  welcomeMessage: [Function: welcomeMessage]
+} */
+console.log(this);
+//{} as no global current context this refer to empty object
+
+/**function chai()
+{
+    let username="subha"
+    console.log(this);
+}
+chai()*/
+//<ref *1> Object [global]
+
+/**now comentaing out the above code  */
+function chai()
+{
+    let username="subha"
+    console.log(this.username);
+}
+chai()
+//op: udefined ..> it will only work inside the object not in function
+
+
+/**const wow=()=>{
+    let username="subha"
+    console.log(this.username);
+}
+wow();*/
+//undefiend
+
+const wow=()=>{
+    let username="subha"
+    console.log(this);
+}
+wow();
+//op: {}
+
+
+//return must in case of curly braces 
+//return not used in case of parenthesis
+
+const wow2=(a)=>({usre: "hello world!!"})
+
+console.log(wow2(2));
+
+//expected output:{ usre: 'hello world!!' }
