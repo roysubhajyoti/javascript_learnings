75e8cb6ff58f33781170edc190d9cfabd4657cee
roysubhajyoti <roysubbhajyoti272@gmail.com>
2024-11-18 00:51:32 +0530

    HoF note add

--- JS/HigherOrderFunction.js
+++ JS/HigherOrderFunction.js
@@ -6,3 +6,21 @@ console.log(
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ")
 );
+
+function a(b) {
+  console.log(typeof b);
+  // console.log(b);
+  b();
+}
+// a({ userName: "anurag", userAge: 50 });
+// a("hello");
+// a([1, 23, 5]); // a is not HOF
+
+// console.dir(a); // it will print like object
+// function is a object
+
+const sayHi = function () {
+  console.log("Say hiiii");
+}; // its a anonymous function
+
+a(sayHi); // now a is A HoF
---
+++ JS/HoF&Callback.md
@@ -0,0 +1,3 @@
+### Higher Order Function & Callback Function:
+
+- when a function passed as an argument to a function that passed function is know as a `callback function` and the caller function know as `Higher Order Function`.
